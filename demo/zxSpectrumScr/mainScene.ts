be94d7a77320b42131dbd47237496d832d59e6bb
VAkozlov <kozlov-victor@rambler.ru>
2019-10-29 13:30:24 +0200

    zxSpectrum scr demo viewer

--- demo/zxSpectrumScr/mainScene.ts
+++ demo/zxSpectrumScr/mainScene.ts
@@ -1,8 +1,13 @@
 import {Scene} from "@engine/scene/scene";
 import {Image} from "@engine/renderable/impl/geometry/image";
-import {Texture} from "@engine/renderer/webGl/base/texture";
 import {WebGlRenderer} from "@engine/renderer/webGl/webGlRenderer";
 import {ScrReader} from "./scrReader";
+import {MOUSE_EVENTS} from "@engine/control/mouse/mouseEvents";
+import {CurtainOpeningTransition} from "@engine/scene/transition/appear/curtainOpeningTransition";
+
+const files =
+    'athena,brunilda,cauldron,dlair,example,Gauntlet,KValley,Phantis,test,wtss'.split(',').map(it=>`./zxSpectrumScr/files/${it}.scr`);
+let ptr = 0;
 
 export class MainScene extends Scene {
 
@@ -13,13 +18,16 @@ export class MainScene extends Scene {
         (this.game.getRenderer() as WebGlRenderer).setPixelPerfectMode(true);
         const img = new Image(this.game);
         this.img = img;
-        const binary = this.resourceLoader.loadBinary('./zxSpectrumScr/files/test.scr');
+        const binary = this.resourceLoader.loadBinary(files[ptr++]);
+        if (ptr>files.length-1) ptr = 0;
         this.resourceLoader.addNextTask(()=>{
             const reader = new ScrReader(this.game,binary.getTarget());
             const link = reader.createTextureLink();
             img.setResourceLink(link);
         });
-
+        this.on(MOUSE_EVENTS.click, ()=>{
+            this.game.runScene(new MainScene(this.game),new CurtainOpeningTransition(this.game));
+        });
     }
 
     public onReady() {
