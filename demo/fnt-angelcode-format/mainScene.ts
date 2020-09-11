f4d3f0f18807fab8d878a4e0773c3416386617fb
Kozlov Victor <kozlovvictorzx@gmail.com>
2020-09-12 00:44:00 +0300

    angelcode-loader.ts, font padding

--- demo/fnt-angelcode-format/mainScene.ts
+++ demo/fnt-angelcode-format/mainScene.ts
@@ -16,12 +16,11 @@ export class MainScene extends Scene {
     public onReady() {
 
         this.colorBG.setRGB(12,12,12);
-
         const tf:TextField = new TextField(this.game,this.fontLink.getTarget());
+        tf.textColor.setRGBA(0,0,0,0);
         tf.size.setWH(300,200);
         tf.setWordBrake(WordBrake.PREDEFINED);
         tf.pos.setY(23);
-        tf.textColor.setRGB(255);
         tf.setText("hello world\nnew string\nКириллица!");
         this.appendChild(tf);
 
