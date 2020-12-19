c2c82f81495093cd04f9cbf0a0be66878653faa1
Kozlov Victor <kozlovvictorzx@gmail.com>
2020-12-19 15:29:53 +0200

    svg image parsing

--- demo/svgBasic2/mainScene.ts
+++ demo/svgBasic2/mainScene.ts
@@ -3,6 +3,9 @@ import * as svgEx1 from "xml/xml-loader!./examples/illustrator-test-1.svg";
 import * as svgEx2 from "xml/xml-loader!./examples/illustrator-test-2.svg";
 import * as svgEx3 from "xml/xml-loader!./examples/illustractor-export-test1.svg";
 import * as svgEx4 from "xml/xml-loader!./examples/illustractor-export-test2.svg";
+import * as svgEx5 from "xml/xml-loader!./examples/heart.svg";
+import * as svgEx6 from "xml/xml-loader!./examples/dragon.svg";
+import * as svgEx7 from "xml/xml-loader!./examples/dragon2.svg";
 
 
 import {Scene} from "@engine/scene/scene";
@@ -11,7 +14,8 @@ import {Element} from "@engine/misc/xmlUtils";
 import {SvgImage} from "../svgBasic/svgImage";
 
 const images:Element[] = [
-    svgEx1,svgEx2, svgEx3, svgEx4,
+    svgEx6,svgEx7,
+    svgEx1,svgEx2, svgEx3, svgEx4, svgEx5,
 ];
 
 export class MainScene extends Scene {
