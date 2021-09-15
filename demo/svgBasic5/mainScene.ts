ca024581968e638ee5e51483cd5d34d46ebebe3a
Kozlov Victor <kozlovvictorzx@gmail.com>
2021-09-15 17:26:20 +0300

    more svg tests

--- demo/svgBasic5/mainScene.ts
+++ demo/svgBasic5/mainScene.ts
@@ -7,7 +7,7 @@ import {XmlParser} from "@engine/misc/xml/xmlParser";
 
 // https://www.javatpoint.com/svg-tutorial
 const imageUrls:string[] = [
-    'info',
+    'info', 'glifs',
     'guide', 'us-bank-icon', 'tide','test-fill-non-zero','test-fill-even-odd',
     '1','2','3', '4', '5', '6', '7', '8', '9', '10',
     '11','12','13', '14', '15', '16', '17', '18', '19', '20',
@@ -25,14 +25,14 @@ export class MainScene extends Scene {
             if (lastImage!==undefined) lastImage.removeSelf();
             const queue = new TaskQueue(this.game);
             const doc = await new ResourceLoader(this.game).loadXML(XmlParser,'./svgBasic5/examples/'+imageUrls[i]+'.svg');
-            lastImage = await SvgImage.create(this.game,queue,doc);
+            lastImage = await SvgImage.create(this.game,queue,doc,this.game.size);
             this.appendChild(lastImage);
             console.log(lastImage);
             i++;
             i = i%imageUrls.length;
         };
 
-        placeNextImage();
+        placeNextImage().catch(e=>console.log(e));
         this.mouseEventHandler.on(MOUSE_EVENTS.click, ()=>placeNextImage());
     }
 }
