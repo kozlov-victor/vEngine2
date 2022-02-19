27a83d346cd044faab4f9524c1a5030bb4050349
Kozlov Victor <kozlovvictorzx@gmail.com>
2022-02-19 13:01:07 +0200

    tileMap collisions, more demos

---
+++ demo/tileMap4/index.ts
@@ -0,0 +1,19 @@
+import {KeyboardControl} from "@engine/control/keyboard/keyboardControl";
+import {Game} from "@engine/core/game";
+import {Scene} from "@engine/scene/scene";
+import {MainScene} from "./mainScene";
+import {WebGlRenderer} from "@engine/renderer/webGl/renderer/webGlRenderer";
+import {ArcadePhysicsSystem} from "@engine/physics/arcade/arcadePhysicsSystem";
+
+const game = new Game({width:1024,height:600});
+game.setRenderer(WebGlRenderer);
+game.getRenderer<WebGlRenderer>().setPixelPerfect(true);
+game.addControl(KeyboardControl);
+ArcadePhysicsSystem.gravity.setXY(0);
+game.setPhysicsSystem(ArcadePhysicsSystem);
+const mainScene: Scene = new MainScene(game);
+game.runScene(mainScene);
+
+
+
+
