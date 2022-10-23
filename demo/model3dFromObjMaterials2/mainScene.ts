1c464b7f5aca0c1494f1989cd191c5663ede9d5c
Kozlov Victor <kozlovvictorzx@gmail.com>
2022-10-23 21:40:09 +0300

    revision

--- demo/model3dFromObjMaterials2/mainScene.ts
+++ demo/model3dFromObjMaterials2/mainScene.ts
@@ -5,6 +5,7 @@ import {SimpleGameObjectContainer} from "@engine/renderable/impl/general/simpleG
 import {ITexture} from "@engine/renderer/common/texture";
 import {MOUSE_EVENTS} from "@engine/control/mouse/mouseEvents";
 import {TrackBall} from "../model3dFromFbx/trackBall";
+import {Model3d} from "@engine/renderable/impl/3d/model3d";
 
 
 export class MainScene extends Scene {
@@ -18,18 +19,33 @@ export class MainScene extends Scene {
     @Resource.Texture('./model3dFromObjMaterials2/windmill/windmill.jpg')
     public readonly texture:ITexture;
 
+    @Resource.Texture('./model3dFromObjMaterials2/windmill/windmill_normals.jpg')
+    public readonly normalsTexture:ITexture;
+
+    @Resource.Texture('./model3dFromObjMaterials2/windmill/windmill_wings.jpg')
+    public readonly wingsTexture:ITexture;
+
+    @Resource.Texture('./model3dFromObjMaterials2/windmill/windmill_wings_normals.jpg')
+    public readonly wingsNormalTexture:ITexture;
+
     public override onReady():void {
 
         const obj:SimpleGameObjectContainer = new ObjParser().parse(this.game,
             {
                 meshData:this.meshData,
                 materialsData:this.materialData,
-                texture:this.texture
+                texture:this.texture,
+                normalsTexture: this.normalsTexture,
             }
         );
         obj.pos.setXY(300,450);
         obj.size.setWH(200,200);
         obj.scale.setXYZ(40);
+
+        const wheel = obj.getChildAt(0) as Model3d;
+        wheel.texture = this.wingsTexture;
+        wheel.normalsTexture = this.wingsNormalTexture;
+
         this.appendChild(obj);
         const timer = this.setInterval(()=>{
             obj.angle3d.y-=0.01;
