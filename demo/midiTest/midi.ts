73dfdef7b942b427c3b1fe91f3f89e644046da6c
Kozlov Victor <kozlovvictorzx@gmail.com>
2022-11-19 18:35:18 +0200

    midi synthesis demo, instruments

--- demo/midiTest/midi.ts
+++ demo/midiTest/midi.ts
@@ -1,5 +1,6 @@
 import {Wave} from "../pix32/ym-player/internal/wav";
-import {MathEx} from "@engine/misc/math/mathEx";
+import {createRange} from "@engine/misc/object";
+import * as ts from "typescript/lib/tsserverlibrary";
 
 
 interface ADSRPoint {
@@ -14,9 +15,15 @@ interface SAMPLE {
     R: number;
 }
 
+interface IADSR {
+    a:number;
+    d:number;
+    s:number;
+    r:number;
+}
+
 
 interface CHANNEL_PRESET {
-    waveForm: WAVE_FORM;
     balance: number;
 }
 
@@ -40,6 +47,8 @@ interface INTERNAL_MIDI_COMMAND {
     note: number;
     velocity: number;
     percussion: boolean;
+    instrumentNumber: number;
+    noteId:number;
 }
 
 export interface IMidiJson {
@@ -75,31 +84,24 @@ export interface IMidiJson {
 const defaultPresets:PRESETS = {
     channels: [
         {
-            waveForm: 'triangle',
             balance: 0.1
         },
         {
-            waveForm: 'triangle',
             balance: 0.2
         },
         {
-            waveForm: 'sin',
             balance: 0.1
         },
         {
-            waveForm: "triangle",
             balance: 0.7
         },
         {
-            waveForm: "sin",
             balance: 0.5
         },
         {
-            waveForm: 'sin',
             balance: 1
         },
         {
-            waveForm: 'sin',
             balance: -0.9
         }
     ]
@@ -165,7 +167,8 @@ class ASDRForm {
     private forceReleaseStartedAt:number;
     public forceRelease:boolean = false; // key is released, so force go to "d" ("delay") segment if ADSR curve
 
-    constructor(a: number, s: number, d: number, r: number) {
+    constructor(adsr:IADSR) {
+        const {a,d,s,r} = adsr;
         this.attack = {from: {time: 0, val: 0}, to: {time: a, val: 1}};
         this.sustain = {from: {time: a, val: 1}, to: {time: a + s, val: 1}};
         this.delay = {from: {time: a + s, val: 1}, to: {time: a + s + d, val: 0.5}};
@@ -221,6 +224,219 @@ const log = (...val:any[])=>{
     console.log(...val);
 }
 
+interface InstrumentSettings {
+    adsr: IADSR;
+    wave: WAVE_FORM;
+}
+
+class Instrument {
+    private defaultInstrumentSettings = {
+        piano: {
+            adsr: {a: 0.02, d: 0.1, s: 0.3, r: 1},
+            wave: 'triangle'
+        } as InstrumentSettings,
+        percussion: {
+            adsr: {a: 0.01, d: 0.05, s: 0.1, r: 0.1},
+            wave: 'noise',
+        } as InstrumentSettings,
+        organ: {
+            adsr: {a: 0.01, d: 0.001, s: 10, r: 2},
+            wave: 'triangle'
+        } as InstrumentSettings,
+        pipe: {
+            adsr: {a: 0.02, d: 0.01, s: 3, r: 0.2},
+            wave: 'triangle'
+        } as InstrumentSettings,
+    }
+
+    /*
+    Piano:
+        1 Acoustic Grand Piano
+        2 Bright Acoustic Piano
+        3 Electric Grand Piano
+        4 Honky-tonk Piano
+        5 Electric Piano 1
+        6 Electric Piano 2
+        7 Harpsichord
+        8 Clavinet
+
+        Chromatic Percussion:
+        9 Celesta
+        10 Glockenspiel
+        11 Music Box
+        12 Vibraphone
+        13 Marimba
+        14 Xylophone
+        15 Tubular Bells
+        16 Dulcimer
+
+        Organ:
+        17 Drawbar Organ
+        18 Percussive Organ
+        19 Rock Organ
+        20 Church Organ
+        21 Reed Organ
+        22 Accordion
+        23 Harmonica
+        24 Tango Accordion
+
+        Guitar:
+        25 Acoustic Guitar (nylon)
+        26 Acoustic Guitar (steel)
+        27 Electric Guitar (jazz)
+        28 Electric Guitar (clean)
+        29 Electric Guitar (muted)
+        30 Overdriven Guitar
+        31 Distortion Guitar
+        32 Guitar harmonics
+
+        Bass:
+        33 Acoustic Bass
+        34 Electric Bass (finger)
+        35 Electric Bass (pick)
+        36 Fretless Bass
+        37 Slap Bass 1
+        38 Slap Bass 2
+        39 Synth Bass 1
+        40 Synth Bass 2
+
+        Strings:
+        41 Violin
+        42 Viola
+        43 Cello
+        44 Contrabass
+        45 Tremolo Strings
+        46 Pizzicato Strings
+        47 Orchestral Harp
+        48 Timpani
+
+        Strings (continued):
+        49 String Ensemble 1
+        50 String Ensemble 2
+        51 Synth Strings 1
+        52 Synth Strings 2
+        53 Choir Aahs
+        54 Voice Oohs
+        55 Synth Voice
+        56 Orchestra Hit
+
+        Brass:
+        57 Trumpet
+        58 Trombone
+        59 Tuba
+        60 Muted Trumpet
+        61 French Horn
+        62 Brass Section
+        63 Synth Brass 1
+        64 Synth Brass 2
+
+        Reed:
+        65 Soprano Sax
+        66 Alto Sax
+        67 Tenor Sax
+        68 Baritone Sax
+        69 Oboe
+        70 English Horn
+        71 Bassoon
+        72 Clarinet
+
+        Pipe:
+        73 Piccolo
+        74 Flute
+        75 Recorder
+        76 Pan Flute
+        77 Blown Bottle
+        78 Shakuhachi
+        79 Whistle
+        80 Ocarina
+
+        Synth Lead:
+        81 Lead 1 (square)
+        82 Lead 2 (sawtooth)
+        83 Lead 3 (calliope)
+        84 Lead 4 (chiff)
+        85 Lead 5 (charang)
+        86 Lead 6 (voice)
+        87 Lead 7 (fifths)
+        88 Lead 8 (bass + lead)
+
+        Synth Pad:
+        89 Pad 1 (new age)
+        90 Pad 2 (warm)
+        91 Pad 3 (polysynth)
+        92 Pad 4 (choir)
+        93 Pad 5 (bowed)
+        94 Pad 6 (metallic)
+        95 Pad 7 (halo)
+        96 Pad 8 (sweep)
+
+        Synth Effects:
+        97 FX 1 (rain)
+        98 FX 2 (soundtrack)
+        99 FX 3 (crystal)
+        100 FX 4 (atmosphere)
+        101 FX 5 (brightness)
+        102 FX 6 (goblins)
+        103 FX 7 (echoes)
+        104 FX 8 (sci-fi)
+
+        Ethnic:
+        105 Sitar
+        106 Banjo
+        107 Shamisen
+        108 Koto
+        109 Kalimba
+        110 Bag pipe
+        111 Fiddle
+        112 Shanai
+
+        Percussive:
+        113 Tinkle Bell
+        114 Agogo
+        115 Steel Drums
+        116 Woodblock
+        117 Taiko Drum
+        118 Melodic Tom
+        119 Synth Drum
+
+        Sound effects:
+        120 Reverse Cymbal
+        121 Guitar Fret Noise
+        122 Breath Noise
+        123 Seashore
+        124 Bird Tweet
+        125 Telephone Ring
+        126 Helicopter
+        127 Applause
+        128 Gunshot
+     */
+    private midiInstrumentsTable = [
+        {
+            range: [...createRange({from: 1, to: 16})],
+            settings: this.defaultInstrumentSettings.piano,
+        },
+        {
+            range: [...createRange({from: 17, to: 24})],
+            settings: this.defaultInstrumentSettings.organ,
+        },
+        {
+            range: [...createRange({from: 65, to: 80})],
+            settings: this.defaultInstrumentSettings.pipe,
+        },
+
+    ]
+
+    public getOscillatorSettingsByMidiInstrumentNumber(num:number,percussion:boolean):InstrumentSettings {
+        if (percussion) return this.defaultInstrumentSettings.percussion;
+        for (const tbl of this.midiInstrumentsTable) {
+            if (tbl.range.includes(num)) return tbl.settings;
+        }
+        return this.defaultInstrumentSettings.piano;
+    }
+
+}
+
+
 class Oscillator {
 
     public velocity: number = 1;
@@ -229,6 +445,7 @@ class Oscillator {
     public adsrForm: ASDRForm;
     public frequency: number = 0;
     public lastTriggeredCommandIndex:number;
+    public currentNoteId:number;
 
     constructor(private tracker:Tracker) {}
 
@@ -294,6 +511,9 @@ export class Tracker {
     private readonly midiNoteToFrequencyTable: number[];
     private sampleToCommandsMap: Record<number, INTERNAL_MIDI_COMMAND[]>;
     private lastEventTime: number;
+    private numOfTracks:number;
+    private nextId:number;
+    private instrument = new Instrument();
     public readonly _oscillators: Oscillator[] = [];
 
     constructor(public readonly sampleRate: number = 11025, private presets: PRESETS = defaultPresets) {
@@ -306,10 +526,12 @@ export class Tracker {
     private _init() {
         this.sampleToCommandsMap = {};
         this.lastEventTime = 0;
+        this.numOfTracks = 0;
+        this.nextId = 0;
     }
 
 
-    private _commandToInternalCommand(time: number, opCode: tOpCode, cmd: MIDI_COMMAND, track:IMidiJson['tracks'][0]): void {
+    private _commandToInternalCommand(noteId:number, instrumentNumber: number, time: number, opCode: tOpCode, cmd: MIDI_COMMAND, track:IMidiJson['tracks'][0]): void {
         const sampleNum = ~~(time * this.sampleRate);
         if (!this.sampleToCommandsMap[sampleNum]) this.sampleToCommandsMap[sampleNum] = [];
         this.sampleToCommandsMap[sampleNum].push({
@@ -318,6 +540,8 @@ export class Tracker {
             track: track.id,
             velocity: cmd.velocity,
             percussion: track.isPercussion===true,
+            noteId,
+            instrumentNumber
         });
         if (this.lastEventTime < sampleNum) this.lastEventTime = sampleNum;
     }
@@ -326,9 +550,12 @@ export class Tracker {
         this._init();
         midiJson.tracks.forEach(t=>{
             if (!t.notes) return;
+            this.numOfTracks++;
+            const instrumentNumber = t.instrumentNumber ?? 1;
             t.notes.forEach(n=>{
-                this._commandToInternalCommand(n.time,'noteOn', n, t);
-                this._commandToInternalCommand(n.time + n.duration, 'noteOff', n, t);
+                const noteId = this.nextId++;
+                this._commandToInternalCommand(noteId, instrumentNumber, n.time,'noteOn', n, t);
+                this._commandToInternalCommand(noteId,instrumentNumber, n.time + n.duration, 'noteOff', n, t);
             });
         });
     }
@@ -347,27 +574,21 @@ export class Tracker {
 
     private execCommand(command: INTERNAL_MIDI_COMMAND,i:number):void {
         if (command.opCode === 'noteOn') {
+
+            const instrumentSettings = this.instrument.getOscillatorSettingsByMidiInstrumentNumber(command.instrumentNumber,command.percussion);
+
             const oscillator = new Oscillator(this);
-            const trackPreset = this.presets.channels[command.track] ||
-                {
-                    velocity: 1,
-                    waveForm: 'triangle',
-                    balance: 0.5
-                };
             oscillator.frequency = this.midiNoteToFrequencyTable[command.note];
             oscillator.velocity = command.velocity;
-            oscillator.waveForm = command.percussion?'noise':trackPreset.waveForm;
-            oscillator.balance = trackPreset.balance;
+            oscillator.waveForm = instrumentSettings.wave;
+            oscillator.balance = this.presets.channels[command.track]?.balance ?? 0.5;
             oscillator.lastTriggeredCommandIndex = i;
-            oscillator.adsrForm =
-                command.percussion?
-                    new ASDRForm(0.01, 0.05, 0.1, 0.1):
-                    new ASDRForm(0.02, 0.1, 0.3, 1)
-            ;
+            oscillator.currentNoteId = command.noteId;
+            oscillator.adsrForm = new ASDRForm(instrumentSettings.adsr);
             this._oscillators.push(oscillator);
         } else if (command.opCode === 'noteOff') {
             for (let i = 0; i < this._oscillators.length; i++) {
-                if (this._oscillators[i].frequency === this.midiNoteToFrequencyTable[command.note]) {
+                if (this._oscillators[i].currentNoteId === command.noteId) {
                     this._oscillators[i].adsrForm.forceRelease = true;
                     break;
                 }
@@ -383,24 +604,17 @@ export class Tracker {
                 this.execCommand(possibleCommands[i],i);
             }
         }
-        // mix by formula: out = (s1 + s2) - (s1 * s2);
+
         const sumAll: SAMPLE = {L: 0, R: 0};
-        const multAll: SAMPLE = {L: 1, R: 1};
         for (let i: number = 0; i < this._oscillators.length; i++) {
             const sample: SAMPLE = this._oscillators[i].generateSample(currentSampleNum);
             sumAll.L += sample.L;
             sumAll.R += sample.R;
-            multAll.L *= sample.L;
-            multAll.R *= sample.R;
         }
 
         const mixed:SAMPLE = {L:0, R:0};
-        //if (sumAll.L!==0) {
-            mixed.L = sumAll.L - Math.abs(multAll.L);
-        //}
-        //if (sumAll.R!==0) {
-            mixed.R = sumAll.R - Math.abs(multAll.R);
-        //}
+        mixed.L = sumAll.L/this.numOfTracks;
+        mixed.R = sumAll.R/this.numOfTracks;
         return mixed;
     }
 
