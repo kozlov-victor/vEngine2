0a9513941abb94cb61c7740367b2e4517b4fe7e7
VAKozlov <VAKozlov@alfabank.ua>
2023-05-05 20:18:54 +0300

    camera follow strategy

--- engine/renderer/camera/follow/scrollFollowStrategy.ts
+++ engine/renderer/camera/follow/scrollFollowStrategy.ts
@@ -3,15 +3,28 @@ import {RenderableModel} from "@engine/renderable/abstract/renderableModel";
 import {Camera} from "@engine/renderer/camera/camera";
 import {Point2d} from "@engine/geometry/point2d";
 import {Game} from "@engine/core/game";
-import {Vec2} from "@engine/geometry/vec2";
+import {ISize} from "@engine/geometry/size";
+
+const clampCameraPos = (pos:Point2d, sceneSize: ISize, gameSize: ISize)=>{
+    if (pos.x < 0) {
+        pos.x = 0;
+    }
+    if (pos.y < 0) {
+        pos.y = 0;
+    }
+
+    if (pos.x > sceneSize.width - gameSize.width) {
+        pos.x = sceneSize.width - gameSize.width;
+    }
+    if (pos.y > sceneSize.height - gameSize.height) {
+        pos.y = sceneSize.height - gameSize.height;
+    }
+}
 
 export class ScrollFollowStrategy implements ICameraFollowStrategy {
 
     private targetObject:RenderableModel|undefined;
     private camera: Camera;
-
-    private _objFollowToPrevPos = new Point2d();
-    private _cameraPosCorrection = new Point2d();
 
     constructor(private game:Game) {}
 
@@ -19,11 +32,12 @@ export class ScrollFollowStrategy implements ICameraFollowStrategy {
         this.targetObject = targetObject;
         this.camera = camera;
         if (targetObject!==undefined) {
-            this._objFollowToPrevPos.setFrom(targetObject.pos);
-            this._cameraPosCorrection.setXY(
+            this.camera.pos.setXY(
                 targetObject.pos.x - this.game.width/2,
                 targetObject.pos.y - this.game.height/2
             );
+            const scene = this.game.getCurrentScene();
+            clampCameraPos(camera.pos,scene.size,this.game.size);
         }
     }
 
@@ -36,57 +50,14 @@ export class ScrollFollowStrategy implements ICameraFollowStrategy {
             const h = this.game.size.height;
             const wDiv2 = w / 2;
             const hDiv2 = h / 2;
-            const wDiv3 = w / 3;
-            const hDiv3 = h / 3;
             const scene = this.game.getCurrentScene();
-            const posCorrection = this._cameraPosCorrection;
+
             const pos = this.camera.pos;
-            const cameraToleranceVal = 15;
-
-            if ((targetObject.pos.x - this._objFollowToPrevPos.x)>cameraToleranceVal) { // camera moves right
-                posCorrection.x = targetObject.pos.x - wDiv2 + wDiv3;
-                this._objFollowToPrevPos.x = targetObject.pos.x;
-            }
-            else if ((targetObject.pos.x - this._objFollowToPrevPos.x)<-cameraToleranceVal) { // camera moves left
-                posCorrection.x = targetObject.pos.x - wDiv2 - wDiv3;
-                this._objFollowToPrevPos.x = targetObject.pos.x;
-            }
-
-            if ((targetObject.pos.y - this._objFollowToPrevPos.y)>cameraToleranceVal) { // camera moves down
-                posCorrection.y = targetObject.pos.y - hDiv2 + hDiv3;
-                this._objFollowToPrevPos.y = targetObject.pos.y;
-            }
-            else if ((targetObject.pos.y - this._objFollowToPrevPos.y)<-cameraToleranceVal) { // camera moves up
-                posCorrection.y = targetObject.pos.y - hDiv2 - hDiv3;
-                this._objFollowToPrevPos.y = targetObject.pos.y;
-            }
-
-            const dist = Vec2.distance(posCorrection,pos);
-            const maxCorrectionLengthForOneFrame = 100;
-            if (dist>maxCorrectionLengthForOneFrame) {
-                const angle = Vec2.angleTo(pos,posCorrection);
-                posCorrection.x = pos.x + maxCorrectionLengthForOneFrame * Math.cos(angle);
-                posCorrection.y = pos.y + maxCorrectionLengthForOneFrame * Math.sin(angle);
-            }
-
-
-            if (posCorrection.x < 0) {
-                posCorrection.x = 0;
-            }
-            if (posCorrection.y < 0) {
-                posCorrection.y = 0;
-            }
-
-            if (posCorrection.x > scene.size.width - w) {
-                posCorrection.x = scene.size.width - w;
-            }
-            if (posCorrection.y > scene.size.height - h) {
-                posCorrection.y = scene.size.height - h;
-            }
-
             const lerp = 0.05;
-            pos.x +=(posCorrection.x - pos.x) * lerp;
-            pos.y +=(posCorrection.y - pos.y) * lerp;
+            pos.x +=(targetObject.pos.x - wDiv2 - pos.x) * lerp;
+            pos.y +=(targetObject.pos.y - hDiv2 - pos.y) * lerp;
+
+            clampCameraPos(pos,scene.size,this.game.size);
 
         }
     }
